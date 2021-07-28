8aebbddacc7bafedf512a27d79058026cbc57f35
dilli4545 <dilli4545ecea@gmail.com>
2021-07-28 23:05:25 +0100

    added few things

--- vaccination/settings.py
+++ vaccination/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nzdgcpd()8&*2j-0!=i&+kczx!l0spkybf^_(5n+4g4ud1d#pz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['120.0.0.1',]
 
 
 # Application definition
